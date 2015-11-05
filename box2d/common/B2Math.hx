8db059f2313e15d9c1b7bacf9be5b6bfed08e497
Daniel Uranga <daniel.uranga@gmail.com>
2015-11-05 01:10:58 -0300

    B2Vec2 "length" method

--- box2d/common/B2Math.hx
+++ box2d/common/B2Math.hx
@@ -9,6 +9,7 @@ extern class B2Vec2 {
 	var x : Float;
 	var y : Float;
 	@:native('b2Vec2') public static function create (x : Float, y : Float) : B2Vec2;
+	@:native("Length") public function length() : Float;
 }
 
 class B2Math {}
