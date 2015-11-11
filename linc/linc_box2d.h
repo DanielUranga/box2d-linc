2f75354706d8cc58fba093a3d95d81a169255dc7
Daniel Uranga <daniel.uranga@gmail.com>
2015-11-11 01:16:16 -0300

    Add Box2D again as a git submodule

--- linc/linc_box2d.h
+++ linc/linc_box2d.h
@@ -1,14 +1,14 @@
 #ifndef _LINC_EMPTY_H_
 #define _LINC_EMPTY_H_
 	
-#include "../lib/box2d/Common/b2Math.h"
-#include "../lib/box2d/Dynamics/b2Body.h"
-#include "../lib/box2d/Dynamics/b2Fixture.h"
-#include "../lib/box2d/Dynamics/b2World.h"
-#include "../lib/box2d/Dynamics/b2WorldCallbacks.h"
+#include "../lib/Box2D/Box2D/Box2D/Common/b2Math.h"
+#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2Body.h"
+#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2Fixture.h"
+#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2World.h"
+#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2WorldCallbacks.h"
 
-#include "../lib/box2d/Collision/Shapes/b2PolygonShape.h"
-#include "../lib/box2d/Collision/Shapes/b2Shape.h"
+#include "../lib/Box2D/Box2D/Box2D/Collision/Shapes/b2PolygonShape.h"
+#include "../lib/Box2D/Box2D/Box2D/Collision/Shapes/b2Shape.h"
 
 #include <hxcpp.h>
 
