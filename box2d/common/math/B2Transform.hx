e2026aa0d5abef516eca1a59db61658640a8a377
Daniel Uranga <daniel.uranga@gmail.com>
2015-12-17 20:39:40 -0300

    bugfix B2Transform

--- box2d/common/math/B2Transform.hx
+++ box2d/common/math/B2Transform.hx
@@ -4,10 +4,13 @@ import cpp.Float32;
 
 @:build(linc.Linc.touch())
 @:include('linc_box2d.h')
-@:native('b2Vec2')
+@:native('b2Transform')
 @:structAccess
 extern class B2Transform {
 	var p : B2Vec2;
 	var q : B2Rot;
 	@:native('b2Transform') public static function create () : B2Transform;
 }
+
+@:native('::cpp::Reference<b2Transform>')
+extern class B2TransformRef extends B2Transform {}
