b95d822b145c4668c1cea88f1a6dfb41aac29097
Daniel Uranga <daniel.uranga@gmail.com>
2015-12-17 20:43:07 -0300

    clean linc_box2d.h

--- linc/linc_box2d.h
+++ linc/linc_box2d.h
@@ -1,22 +1,14 @@
 #ifndef _LINC_EMPTY_H_
 #define _LINC_EMPTY_H_
-	
-#include "../lib/Box2D/Box2D/Box2D/Common/b2Math.h"
-#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2Body.h"
-#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2Fixture.h"
-#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2World.h"
-#include "../lib/Box2D/Box2D/Box2D/Dynamics/b2WorldCallbacks.h"
 
-#include "../lib/Box2D/Box2D/Box2D/Collision/Shapes/b2PolygonShape.h"
-#include "../lib/Box2D/Box2D/Box2D/Collision/Shapes/b2Shape.h"
-
-#include <hxcpp.h>
+#include "Box2D/Box2D.h"
 
 namespace linc {
 
 	namespace box2d {
-	} //empty namespace
 
-} //linc
+	}
+
+}
 
 #endif //_LINC_EMPTY_H_
