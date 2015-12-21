cc7997a6a6ddfc5ac809ecbc2367397c0090dbf1
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-12-21 16:56:48 -0300

    Add drawSolidCircle missing binding & remove un-needed c++-11 "override" specifiers.

--- box2d/common/B2DebugDrawNative.cpp
+++ box2d/common/B2DebugDrawNative.cpp
@@ -14,27 +14,27 @@ public:
 		GCRemoveRoot((hx::Object**)&(haxeDelegate.mPtr));
 	}
 
-	void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override {
+	void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) {
 		haxeDelegate->drawPolygon(vertices, vertexCount, &color);
 	}
 
-	void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override {
+	void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) {
 		haxeDelegate->drawSolidPolygon(vertices, vertexCount, &color);
 	}
 
-	void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color) override {
+	void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color) {
 		haxeDelegate->drawCircle(&center, radius, &color);
 	}
 
-	void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color) override {
-
+	void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color) {
+		haxeDelegate->drawSolidCircle(&center, radius, &axis, &color);
 	}
 
-	void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) override {
+	void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) {
 		haxeDelegate->drawSegment(&p1, &p2, &color);
 	}
 
-	void DrawTransform(const b2Transform& xf) override {
+	void DrawTransform(const b2Transform& xf) {
 		haxeDelegate->drawTransform(&xf);
 	}
 
